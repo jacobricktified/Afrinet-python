e3e4c5d28cff11eb1b4bf7f2e5be2235e01db5c0
jacob gichira mbuco <mbucojacob@gmail.com>
2025-12-02 11:14:49 +0300

    Ask user of his grade print passed if it is 70-75.

--- Python Bootcamp/ifstatement.py
+++ Python Bootcamp/ifstatement.py
@@ -1,31 +1,38 @@
-num1=2
-if num1==2:
-    print("true")
-elif num1 ==0:
-    print ("this is zero")
-else:
-    print("false")
+# num1=2
+# if num1==2:
+#     print("true")
+# elif num1 ==0:
+#     print ("this is zero")
+# else:
+#     print("false")
 
-num2=10
-if num2==10:
-    print("you are number 10 ")
-else:
-    print("you are not number 10")
-age =input("Enter your age: ")
-age = int(age)
-if age >=18:
-    print("you are an adult")
-elif age <18 and age >0:
-    print("you are a minor")
-else:
-    print("Age not identified")
+# num2=10
+# if num2==10:
+#     print("you are number 10 ")
+# else:50
+#     print("you are not number 10")
+# age =input("Enter your age: ")
+# age = int(age)
+# if age >=18:
+#     print("you are an adult")
+# elif age <18 and age >0:
+#     print("you are a minor")
+# else:
+#     print("Age not identified")
     
-num3 = input("Enter first number: ")
-num4 = input("Enter second number: ")   
-num3 = int(num3)
-num4 = int(num4)
-sum_numbers = num3 + num4
-if sum_numbers > 20:
-    print("The sum is greater than 20")
+# num3 = input("Enter first number: ")
+# num4 = input("Enter second number: ")   
+# num3 = int(num3)
+# num4 = int(num4)
+# sum_numbers = num3 + num4
+# if sum_numbers > 20:
+#     print("The sum is greater than 20")
+# else:
+#     print("The sum is not greater than 20") 
+#ask user to enter grade check if grade is between 70 to75 is passed elese failed
+grade = input("Enter your grade: ")
+grade = int(grade)  
+if grade >= 70 and grade <= 75:
+    print("Passed")
 else:
-    print("The sum is not greater than 20") 
+    print("Failed")     
