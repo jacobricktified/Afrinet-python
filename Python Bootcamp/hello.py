9c5dcdd23bda7f2bf36f8a62286bea02cf32e98f
jacob gichira mbuco <mbucojacob@gmail.com>
2025-12-02 10:13:33 +0300

    Initial commit.

---
+++ Python Bootcamp/hello.py
@@ -0,0 +1,9 @@
+print ("hello Afrinet")
+print ("hello world")
+# This is a simple Python script that prints greetings to the console.
+"""
+this is a multiline comment
+that provides additional information about the script.
+
+
+"""
---
+++ Python Bootcamp/variable.py
@@ -0,0 +1,12 @@
+name = "Afrinet bootcamp"
+print (name)
+Age = 25
+print (Age)
+pin = input("Enter your pin: ")
+print(pin)
+names=input("Enter your names: ")
+print(names)
+number1 =   int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+sum = number1 + number2 
+print("The sum is:", sum)   
