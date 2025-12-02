6585609d63848d2da49e48745b6ecc53d885965f
jacob gichira mbuco <mbucojacob@gmail.com>
2025-12-02 10:58:05 +0300

    Ask user for input ,add the numbeers then check if sum is greater than 20.

--- Python Bootcamp/ifstatement.py
+++ Python Bootcamp/ifstatement.py
@@ -19,3 +19,13 @@ elif age <18 and age >0:
     print("you are a minor")
 else:
     print("Age not identified")
+    
+num3 = input("Enter first number: ")
+num4 = input("Enter second number: ")   
+num3 = int(num3)
+num4 = int(num4)
+sum_numbers = num3 + num4
+if sum_numbers > 20:
+    print("The sum is greater than 20")
+else:
+    print("The sum is not greater than 20") 
