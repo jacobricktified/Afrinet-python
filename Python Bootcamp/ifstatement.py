bf9f9aa39e0e589a5af749c1ced2f7b750d1348a
jacob gichira mbuco <mbucojacob@gmail.com>
2025-12-02 10:50:03 +0300

    Ask user for age then compare age category.

--- Python Bootcamp/ifstatement.py
+++ Python Bootcamp/ifstatement.py
@@ -6,8 +6,16 @@ elif num1 ==0:
 else:
     print("false")
 
-num2=10 
+num2=10
 if num2==10:
     print("you are number 10 ")
 else:
     print("you are not number 10")
+age =input("Enter your age: ")
+age = int(age)
+if age >=18:
+    print("you are an adult")
+elif age <18 and age >0:
+    print("you are a minor")
+else:
+    print("Age not identified")
