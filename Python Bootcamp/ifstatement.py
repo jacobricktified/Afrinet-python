bcce4d7ae7f02d91e501fb8a4fe5ea9cd6d39b1d
jacob gichira mbuco <mbucojacob@gmail.com>
2025-12-02 10:36:23 +0300

    Use a else to check alternative of if.

--- Python Bootcamp/ifstatement.py
+++ Python Bootcamp/ifstatement.py
@@ -1,7 +1,11 @@
 num1=2
 if num1==2:
     print("true")
+else:
+    print("false")
 
 num2=10 
 if num2==10:
-    print("you are number 10 10")
+    print("you are number 10 ")
+else:
+    print("you are not number 10")
